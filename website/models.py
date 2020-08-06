57caa7122550942c5ce1e207641873dcccf76fd2
Nelson Kshetrimayum <nelsonksh.amo@gmail.com>
2020-08-06 08:29:15 +0530

    sample 3 for testing form validity

--- website/models.py
+++ website/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Costumer(models.Model):
     host_name = models.CharField(max_length=50)
-    mobile_number = models.IntegerField()
+    mobile_number = models.CharField(max_length=10)
     email = models.EmailField(max_length=50)
     from_kakching = models.BooleanField()
     address = models.CharField(max_length=200)
